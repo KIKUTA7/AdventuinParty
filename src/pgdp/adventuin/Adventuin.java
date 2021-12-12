cd072404784b29a7136de5ef0a39657c85e19fe8
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-12-12 17:11:11 +0400

    waviwiet win

--- src/pgdp/adventuin/Adventuin.java
+++ src/pgdp/adventuin/Adventuin.java
@@ -4,14 +4,14 @@ import pgdp.color.RgbColor;
 
 public class Adventuin {
     private final String name ;
-    private final int size;
+    private final int height;
     private final RgbColor color;
     private final HatType hatType;
     private final Language language;
     public Adventuin(String name,int size,RgbColor color , HatType hatType, Language language )
     {
         this.name = name;
-        this.size = size;
+        this.height = size;
         this.color = color;
         this.hatType = hatType;
         this.language = language;
@@ -26,7 +26,7 @@ public class Adventuin {
     }
 
     public int getHeight() {
-        return size;
+        return height;
     }
 
     public Language getLanguage() {
