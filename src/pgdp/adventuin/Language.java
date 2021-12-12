fe46c8fb672e7e5ee67208eb529ce26b91e41360
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-12-12 17:06:47 +0400

    waviwiet win

--- src/pgdp/adventuin/Language.java
+++ src/pgdp/adventuin/Language.java
@@ -1,10 +1,13 @@
 package pgdp.adventuin;
 
 public enum Language {
-    GERMAN,ENGLISH;
+    GERMAN,ENGLISH,ARMENIAN;
     public String getLocalizedChristmasGreeting (String greeterName)
     {
-        if(this == GERMAN) return "Fröhliche Weihnachten wünscht dir"+ " " + greeterName + "!";
-        else  return greeterName + " " + "wishes you a Merry Christmas!";
+        if(this == GERMAN) return "Fröhliche Weihnachten wünscht dir "+  greeterName + "!";
+        else if(this == ENGLISH) return greeterName + " wishes you a Merry Christmas!";
+        else {
+            return "Շնորհավոր Նոր Տարի" + " " + greeterName + "!";
+        }
     }
 }
