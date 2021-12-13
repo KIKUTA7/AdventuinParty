bcdb867065e9a4782ca2cec23a14eb745ee3e5a5
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-12-13 17:34:31 +0400

    waviwiet win

--- src/pgdp/adventuin/AdventuinParty.java
+++ src/pgdp/adventuin/AdventuinParty.java
@@ -8,39 +8,39 @@ import java.util.HashMap;
 
 public final class AdventuinParty {
 
-//    public static Map<HatType,List<Adventuin>> groupByHatType (List<Adventuin> adventuins)
-//    {
-//        List <Adventuin> santa = new ArrayList<>();
-//        List <Adventuin> reindeer = new ArrayList<>();
-//        List <Adventuin> fishy = new ArrayList<>();
-//        List <Adventuin> nohat = new ArrayList<>();
-//
-//        List <Adventuin> adventuins1= adventuins.subList(0,adventuins.size() - 1);
-//        while (!adventuins1.isEmpty())
-//        {
-//            Adventuin adventuin = adventuins1.remove(adventuins1.size() - 1);
-//            switch (adventuin.getHatType())
-//            {
-//                case SANTA_CLAUS : {santa.add(adventuin);break;}
-//                case FISHY_HAT : {fishy.add(adventuin);break;}
-//                case REINDEER : {reindeer.add(adventuin);break;}
-//                case NO_HAT : {nohat.add(adventuin);break;}
-//            }
-//
-//
-//        }
-//        Map<HatType,List<Adventuin>> ans  = new HashMap<>();
-//        ans.put(HatType.FISHY_HAT, fishy);
-//        ans.put(HatType.NO_HAT, nohat);
-//        ans.put(HatType.REINDEER, reindeer);
-//        ans.put(HatType.SANTA_CLAUS,santa);
-//        return ans;
-//
-//    }
-//    public static void printLocalizedChristmasGreetings (List<Adventuin> adventuins)
-//    {
-//        adventuins.forEach(adventuin -> {System.out.println(adventuin.getLanguage().getLocalizedChristmasGreeting(adventuin.getName()));});
-//    }
+    public static Map<HatType,List<Adventuin>> groupByHatType (List<Adventuin> adventuins)
+    {
+        List <Adventuin> santa = new ArrayList<>();
+        List <Adventuin> reindeer = new ArrayList<>();
+        List <Adventuin> fishy = new ArrayList<>();
+        List <Adventuin> nohat = new ArrayList<>();
+
+        List <Adventuin> adventuins1= adventuins.subList(0,adventuins.size() - 1);
+        while (!adventuins1.isEmpty())
+        {
+            Adventuin adventuin = adventuins1.remove(adventuins1.size() - 1);
+            switch (adventuin.getHatType())
+            {
+                case SANTA_CLAUS : {santa.add(adventuin);break;}
+                case FISHY_HAT : {fishy.add(adventuin);break;}
+                case REINDEER : {reindeer.add(adventuin);break;}
+                case NO_HAT : {nohat.add(adventuin);break;}
+            }
+
+
+        }
+        Map<HatType,List<Adventuin>> ans  = new HashMap<>();
+        ans.put(HatType.FISHY_HAT, fishy);
+        ans.put(HatType.NO_HAT, nohat);
+        ans.put(HatType.REINDEER, reindeer);
+        ans.put(HatType.SANTA_CLAUS,santa);
+        return ans;
+
+    }
+    public static void printLocalizedChristmasGreetings (List<Adventuin> adventuins)
+    {
+        adventuins.stream().sorted(Comparator.comparingInt(Adventuin::getHeight)).forEach(adventuin -> {System.out.println(adventuin.getLanguage().getLocalizedChristmasGreeting(adventuin.getName()));});
+    }
     public static Map<HatType, List<Adventuin>> getAdventuinsWithLongestNamesByHatType (List<Adventuin> adventuins)
     {
         Map<HatType,List<Adventuin>> ans = new HashMap<>();
@@ -50,24 +50,24 @@ public final class AdventuinParty {
         ans.put(HatType.REINDEER,adventuins.stream().filter(adventuin -> adventuin.getHatType() == HatType.REINDEER).max(Comparator.comparingInt(p -> p.getName().length())).stream().toList());
         return ans;
     }
-//    public static   Map<Integer, Double> getAverageColorBrightnessByHeight (List<Adventuin> adventuins)
-//    {
-//        Map<Integer,Double> ans = new HashMap<>();
-//        adventuins.forEach(adventuin -> {
-//            Integer h = new AdventuinParty().round(adventuin.getHeight());
-//            ans.put(h, ((adventuin.getColor().getRed()*0.2126+adventuin.getColor().getGreen()*0.7152+adventuin.getColor().getBlue()*0.0722)/255));
-//        });
-//        return ans;
-//    }
-//    public Integer round (Integer i)
-//    {
-//        if(i%10>=5) return i-i%10+10;
-//        else return i-i%10;
-//    }
-//    public static Map<HatType, Double> getDiffOfAvgHeightDiffsToPredecessorByHatType (List<Adventuin> adventuins)
-//    {
-//        return new HashMap<>();
-//    }
+    public static   Map<Integer, Double> getAverageColorBrightnessByHeight (List<Adventuin> adventuins)
+    {
+        Map<Integer,Double> ans = new HashMap<>();
+        adventuins.forEach(adventuin -> {
+            Integer h = new AdventuinParty().round(adventuin.getHeight());
+            ans.put(h, ((adventuin.getColor().getRed()*0.2126+adventuin.getColor().getGreen()*0.7152+adventuin.getColor().getBlue()*0.0722)/255));
+        });
+        return ans;
+    }
+    public Integer round (Integer i)
+    {
+        if(i%10>=5) return i-i%10+10;
+        else return i-i%10;
+    }
+    public static Map<HatType, Double> getDiffOfAvgHeightDiffsToPredecessorByHatType (List<Adventuin> adventuins)
+    {
+        return new HashMap<>();
+    }
     public static void main(String[] args) {
         HatType hat  =  HatType.FISHY_HAT;
         Language lan = Language.GERMAN;
@@ -78,11 +78,12 @@ public final class AdventuinParty {
         a1.add(a);
         a1.add(aa);
         System.out.println(a1.size());
-//        AdventuinParty party = new AdventuinParty();
-//        party.printLocalizedChristmasGreetings(a1);
-//        Map<HatType,List<Adventuin>> m = party.groupByHatType(a1);
-//        System.out.println(m.get(HatType.FISHY_HAT).size());
-//        Map<HatType,List<Adventuin>> m1 = party.getAdventuinsWithLongestNamesByHatType(a1);
+        AdventuinParty party = new AdventuinParty();
+        party.printLocalizedChristmasGreetings(a1);
+        Map<HatType,List<Adventuin>> m = party.groupByHatType(a1);
+        System.out.println(m.get(HatType.FISHY_HAT).size());
+        Map<HatType,List<Adventuin>> m1 = party.getAdventuinsWithLongestNamesByHatType(a1);
+
 
 
     }
