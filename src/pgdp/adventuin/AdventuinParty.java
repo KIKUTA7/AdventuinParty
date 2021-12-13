7b8afad4d805e9f2674b569586bede67c3f69756
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-12-13 17:35:45 +0400

    waviwiet win

--- src/pgdp/adventuin/AdventuinParty.java
+++ src/pgdp/adventuin/AdventuinParty.java
@@ -53,7 +53,7 @@ public final class AdventuinParty {
     public static   Map<Integer, Double> getAverageColorBrightnessByHeight (List<Adventuin> adventuins)
     {
         Map<Integer,Double> ans = new HashMap<>();
-        adventuins.forEach(adventuin -> {
+        adventuins.stream().sorted(Comparator.comparingInt(Adventuin::getHeight)).forEach(adventuin -> {
             Integer h = new AdventuinParty().round(adventuin.getHeight());
             ans.put(h, ((adventuin.getColor().getRed()*0.2126+adventuin.getColor().getGreen()*0.7152+adventuin.getColor().getBlue()*0.0722)/255));
         });
