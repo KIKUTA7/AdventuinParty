0869ca29f0af814f012b42b55bb4d39de480c7c4
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-12-13 14:37:13 +0400

    waviwiet win

--- src/pgdp/adventuin/AdventuinParty.java
+++ src/pgdp/adventuin/AdventuinParty.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
+
 public final class AdventuinParty {
 
     public static Map<HatType,List<Adventuin>> groupByHatType (List<Adventuin> adventuins)
@@ -50,7 +51,17 @@ public final class AdventuinParty {
     }
     public static   Map<Integer, Double> getAverageColorBrightnessByHeight (List<Adventuin> adventuins)
     {
-        return new HashMap<>();
+        Map<Integer,Double> ans = new HashMap<>();
+        adventuins.forEach(adventuin -> {
+            Integer h = new AdventuinParty().round(adventuin.getHeight());
+            ans.put(h, ((adventuin.getColor().getRed()*0.2126+adventuin.getColor().getGreen()*0.7152+adventuin.getColor().getBlue()*0.0722)/255));
+        });
+        return ans;
+    }
+    public Integer round (Integer i)
+    {
+        if(i%10>=5) return i-i%10+10;
+        else return i-i%10;
     }
     public static Map<HatType, Double> getDiffOfAvgHeightDiffsToPredecessorByHatType (List<Adventuin> adventuins)
     {
@@ -61,12 +72,16 @@ public final class AdventuinParty {
         Language lan = Language.GERMAN;
         RgbColor col = new RgbColor(8,255,255,255);
         Adventuin a = new Adventuin("beq",27,col,hat,lan);
+        Adventuin aa = new Adventuin("beqa",27,col,hat,Language.ENGLISH);
         List<Adventuin> a1 = new ArrayList<Adventuin>();
         a1.add(a);
-        a1.add(a);
+        a1.add(aa);
+        System.out.println(a1.size());
         AdventuinParty party = new AdventuinParty();
+        party.printLocalizedChristmasGreetings(a1);
         Map<HatType,List<Adventuin>> m = party.groupByHatType(a1);
         System.out.println(m.get(HatType.FISHY_HAT).size());
+        Map<HatType,List<Adventuin>> m1 = party.getAdventuinsWithLongestNamesByHatType(a1);
 
 
     }
