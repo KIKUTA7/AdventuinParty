4dcf4956bc5824958b9e71fca32874b12c7beab6
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-12-14 11:09:39 +0400

    waviwiet win

--- src/pgdp/adventuin/AdventuinParty.java
+++ src/pgdp/adventuin/AdventuinParty.java
@@ -139,12 +139,12 @@ public final class AdventuinParty {
               Double resultat = 0.0;
               Double mult = -1.0;
               if (Math.abs(negativeQuantity-0.0000) > .00001) {}
-//                  resultat = resultat + (Double) ((mult) * (double) negativeSum / negativeQuantity);
+                  resultat = resultat + (Double) ((mult) * (Double)  negativeSum / negativeQuantity);
               if (Math.abs(positiveQuantity-0.0000) > .00001) {}
-//                  resultat = resultat + (Double) ((double) positiveSum / positiveQuantity);
+                  resultat = resultat + (Double) ((Double) positiveSum / positiveQuantity);
 
 
-                  ans.put(key, Math.max(resultat, 0.0));
+                  ans.put(key, (Double) Math.max(resultat, 0.0));
 
               }
           }
